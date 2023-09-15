df43ac8e884115508d7b5c32d46446220d3b32d2
fashne <faseeh.jafar@devsinc.com>
2023-09-16 04:01:14 +0500

    new job page and new template page - some issues in breadcrumb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './components/table/table.component';
 import { CategoryComponent } from './components/templates/templates.component';
 import { JobsComponent } from './components/jobs/jobs.component';
+import { CreateTemplateComponent } from './components/create-template/create-template.component';
 
 // configuring routes
 const routes: Routes = [
   { path: 'templates/category', component: CategoryComponent, data: { breadcrumb: 'Category' } },
+  { path: 'templates/category/create_template', component: CreateTemplateComponent, data: { breadcrumb: 'Category/Create Template' } },
   { path: 'jobs', component: JobsComponent, data: { breadcrumb: 'Jobs' } },
 ];
 
--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BreadcrumbService } from 'src/app/services/breadcrumb.service'
 import { BreadcrumbItem } from 'src/app//models/breadcrumb_model';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -10,13 +12,47 @@ import { Observable } from 'rxjs';
 })
 export class BreadcrumbComponent implements OnInit {
 
-
+  private dataSubscription: Subscription = new Subscription;
   breadcrumbs: Observable<BreadcrumbItem[]> = new Observable<BreadcrumbItem[]>();
 
-  constructor(private breadcrumbService: BreadcrumbService) { }
+  constructor(private breadcrumbService: BreadcrumbService, private router: Router) { }
 
   ngOnInit(): void {
     this.breadcrumbs = this.breadcrumbService.breadcrumbs$;
+  }
+  ngOnDestroy(): void {
+
+  }
+
+  addClicked(): void {
+
+    // Check if any breadcrumb's label contains "category"
+    this.breadcrumbs
+      .pipe(
+        map((items) => items.some((breadcrumb) => breadcrumb.label.toLowerCase().includes('category')))
+      )
+      .subscribe((containsCategory) => {
+        if (containsCategory) {
+          this.router.navigateByUrl('templates/category/create_template');
+        } else {
+          console.log('No breadcrumb contains "category".');
+        }
+      });
+
+    // Check if any breadcrumb's label contains "job"
+    this.breadcrumbs
+      .pipe(
+        map((items) => items.some((breadcrumb) => breadcrumb.label.toLowerCase().includes('job')))
+      )
+      .subscribe((containsJob) => {
+        if (containsJob) {
+          this.router.navigateByUrl('jobs/create_job');
+        } else {
+          console.log('No breadcrumb contains "job".');
+        }
+      });
+
+    console.log(this.breadcrumbs);
   }
 
 }
